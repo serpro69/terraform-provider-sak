e342b498c91bd1c125f90ed4c905f06a2dd5af1c
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-05-26 10:54:01 +0200

    Remove unused code

--- internal/provider/provider_test.go
+++ internal/provider/provider_test.go
@@ -5,11 +5,9 @@ package provider_test
 
 import (
 	"terraform-provider-sak/internal/provider"
-	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
-	"github.com/hashicorp/terraform-plugin-testing/echoprovider"
 )
 
 // testAccProtoV6ProviderFactories is used to instantiate a provider during acceptance testing.
@@ -23,13 +21,13 @@ var testAccProtoV6ProviderFactories = map[string]func() (tfprotov6.ProviderServe
 // It allows for testing assertions on data returned by an ephemeral resource during Open.
 // The echoprovider is used to arrange tests by echoing ephemeral data into the Terraform state.
 // This lets the data be referenced in test assertions with state checks.
-var testAccProtoV6ProviderFactoriesWithEcho = map[string]func() (tfprotov6.ProviderServer, error){
-	"sak":  providerserver.NewProtocol6WithError(provider.New("test")()),
-	"echo": echoprovider.NewProviderServer(),
-}
+// var testAccProtoV6ProviderFactoriesWithEcho = map[string]func() (tfprotov6.ProviderServer, error){
+// 	"sak":  providerserver.NewProtocol6WithError(provider.New("test")()),
+// 	"echo": echoprovider.NewProviderServer(),
+// }
 
-func testAccPreCheck(t *testing.T) {
-	// You can add code here to run prior to any test case execution, for example assertions
-	// about the appropriate environment variables being set are common to see in a pre-check
-	// function.
-}
+// func testAccPreCheck(t *testing.T) {
+// 	// You can add code here to run prior to any test case execution, for example assertions
+// 	// about the appropriate environment variables being set are common to see in a pre-check
+// 	// function.
+// }
